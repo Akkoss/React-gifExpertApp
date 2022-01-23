95bd6efd5fb94f9b35a47d3f0e256035870b8200
Maximiliano Ciccarelli <maxiblogss@gmail.com>
2022-01-23 20:15:14 -0300

    haciendo fuincionar el build en git

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,5 +1,4 @@
-import react from "react";
-import { useState } from "react/cjs/react.development";
+import react,{ useState } from "react";
 import PropTypes from 'prop-types';
 
 export const AddCategory = ({setCategories}) =>{
